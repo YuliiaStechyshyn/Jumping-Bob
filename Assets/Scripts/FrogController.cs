4cbfedd4d0cd345b25375965b1089882c0f494c4
Yulia <yulichka211995@gmail.com>
2017-06-30 18:56:30 +0200

    Implementation of game logics

---
+++ Assets/Scripts/FrogController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogController : MonoBehaviour
+{
+
+    public float jumpSpeed = 5f;//or whatever you want it to be
+    public Rigidbody rb; //and again, whatever you want to call it
+    public float  forward ;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+
+      if (Input.GetButton("Fire1"))
+        {
+            rb.velocity = new Vector3(-3, 3, 0);
+           
+        }
+
+    }
+  
+
+
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Jumpimg Bob");
+    }
+
+    public void LoadNextScene()
+    {
+        var current = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(current + 1);
+    }
+}
